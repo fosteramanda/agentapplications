be947af972749857bc8f7fda4b551669990dba21
Amanda Foster <29870948+fosteramanda@users.noreply.github.com>
2025-11-05 11:37:12 -0800

    Update agentapplication-responses.py

--- agentapplication-responses.py
+++ agentapplication-responses.py
@@ -1,1 +1,14 @@
+from openai import OpenAI 
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider 
 
+# edit base_url with your <foundry-resource-name>, <project-name>, and <app-name>
+openai = OpenAI(
+    api_key=get_bearer_token_provider(DefaultAzureCredential(), "https://ai.azure.com/.default"),
+    base_url="https://<foundry-resource-name>.services.ai.azure.com/api/projects/<project-name>/applications/<app-name>/protocols/openai",
+    default_query = {"api-version": "2025-11-15-preview"}
+)
+
+response = openai.responses.create( 
+  input="Write a haiku", 
+) 
+print(f"Response output: {response.output_text}")
